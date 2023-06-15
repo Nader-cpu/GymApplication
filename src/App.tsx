3edd237af10216cc31bcae2bb09a9f2ada7e4046
Nader Nawfal <nader.nauf@gmail.com>
2023-06-15 13:19:56 +0300

    Changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,46 @@
 import GenericDropdown from "./components/dropdown";
 import GenericTable from "./components/dataTable";
-
+import "./styles/App.css";
 import data from "./components/data";
 import GenericInstructions from "./components/GenericInstructions";
 
 function App() {
-	return (
-		<>
-			<div
-				style={{
-					display: "flex",
-					justifyContent: "center",
-				}}>
-				<h2>Create Program</h2>
-			</div>
-			<div
-				style={{
-					display: "flex",
-					justifyContent: "center",
-					alignItems: "center",
-					width: "100vw",
-					overflow: "wrap",
-				}}>
-				<GenericDropdown placeHolder={"Biceps"} exercises={data.biceps} />
-				<GenericDropdown placeHolder={"Triceps"} exercises={data.triceps} />
-				<GenericDropdown placeHolder={"Back"} exercises={data.back} />
-				<GenericDropdown placeHolder={"Chest"} exercises={data.chest} />
-				<GenericDropdown placeHolder={"Legs"} exercises={data.legs} />
-				<GenericDropdown placeHolder={"Shoulders"} exercises={data.shoulders} />
-				<GenericDropdown placeHolder={"Abs"} exercises={data.abs} />
-				<GenericDropdown placeHolder={"Traps"} exercises={data.traps} />
-			</div>
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <h2>Create Program</h2>
+      </div>
+      <div
+        id="container"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100vw",
+          overflow: "wrap",
+        }}
+      >
+        <GenericDropdown placeHolder={"Biceps"} exercises={data.biceps} />
+        <GenericDropdown placeHolder={"Triceps"} exercises={data.triceps} />
+        <GenericDropdown placeHolder={"Back"} exercises={data.back} />
+        <GenericDropdown placeHolder={"Chest"} exercises={data.chest} />
+        <GenericDropdown placeHolder={"Legs"} exercises={data.legs} />
+        <GenericDropdown placeHolder={"Shoulders"} exercises={data.shoulders} />
+        <GenericDropdown placeHolder={"Abs"} exercises={data.abs} />
+        <GenericDropdown placeHolder={"Traps"} exercises={data.traps} />
+      </div>
 
-			<br />
-			<GenericInstructions />
+      <br />
+      <GenericInstructions />
 
-			<GenericTable />
-		</>
-	);
+      <GenericTable />
+    </>
+  );
 }
 
 export default App;
